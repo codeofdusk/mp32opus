1b768574553d0c2b441cf047083b9fd75cd52a13
Bill Dengler <codeofdusk@gmail.com>
2017-03-11 00:28:29 +0000

    Add dir2opus.sh utility script.

---
+++ dir2opus.sh
@@ -0,0 +1,6 @@
+#Convert a directory to Opus.
+#First argument is the directory to convert, second is number of parallel encodes.
+#This script requires mp32opus.sh and Gnu Parallel.
+scriptdir=$(dirname "$0")
+cd "$1"
+ls -1 | grep -i .opus | parallel --jobs $2 "$scriptdir/mp32opus.sh"
